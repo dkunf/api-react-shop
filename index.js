19c02ce6d58ecad674caec9c20496a9966b7580d
doniklt <dkunfchaworl@gmail.com>
2024-03-21 22:36:40 +0200

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 require("dotenv").config();
 
 const cors = require("cors");
